d6d10fb0ed6d60b94f356b4af291a551cb11df96
Satish <satish64607@gmail.com>
2025-11-19 11:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (76.94%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
 
-        int row=mat.size();
-        int col=mat[0].size();
+        int matRows=mat.size();
+        int matCols=mat[0].size();
 
-        if(row*col!=r*c)
+        if(matRows*matCols!=r*c)
             return mat;
 
-         if(row==r && col== c)
+         if(matRows==r && matCols== c)
             return mat;
    
         vector<vector<int>> v(r, vector<int>(c, 0));
-        int newrow =0,newcol = 0;
+       /* int newrow =0,newcol = 0;
 
-        for(int i=0; i<row; i++)
+        for(int i=0; i<matRows; i++)
         {
-            for(int j=0;j<col; j++)
+            for(int j=0;j<matCols; j++)
             {
                 v[newrow][newcol] = mat[i][j];
 
@@ -30,6 +30,19 @@ public:
                     newcol = newcol+1;
                 }
             }
+        }
+
+        return v;*/
+
+        int matPos = 0;
+        int newMatPos = 0;
+
+//though r*c is same as row*col, both have been written just to tell matPs belong to row,col and newMatPos belong to r,c
+        while(matPos<matRows*matCols && newMatPos < r*c) 
+        {
+            v[newMatPos/c][newMatPos%c] = mat[matPos/matCols][matPos%matCols];
+            matPos++;
+            newMatPos++;;
         }
 
         return v;
