51681077d7692be591a9f1794d7ddfa7010f7a26
Satish <satish64607@gmail.com>
2025-11-21 23:25:32 +0530

    Time: 3 ms (47.14%) | Memory: 8 MB (32.77%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+
+        int numMatches = 0;
+
+            while(n)
+            {
+                  if(n%2 == 0)
+                  {
+                        numMatches += n/2;
+                        n=n/2;
+                  }
+                  else
+                  {
+                        numMatches += n/2;
+                        n=n/2 + 1;
+                  }
+                  if(n==1)break;
+            }
+            return numMatches;
+        
+    }
+};
