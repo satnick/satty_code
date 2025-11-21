4f79c6133be1822689d59ebc641453294f86fe59
Satish <satish64607@gmail.com>
2025-11-22 00:24:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (77.60%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.cpp
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        int x1_p1 = rec1[0];
-          int y1_p1 = rec1[1];
-          int x2_p1 = rec1[2];
-          int y2_p1 = rec1[3];
+        int x1_r1 = rec1[0];
+          int y1_r1 = rec1[1];
+          int x2_r1 = rec1[2];
+          int y2_r1 = rec1[3];
 
-          int x1_p2 = rec2[0];
-          int y1_p2 = rec2[1];
-          int x2_p2 = rec2[2];
-          int y2_p2 = rec2[3];
+          int x1_r2 = rec2[0];
+          int y1_r2 = rec2[1];
+          int x2_r2 = rec2[2];
+          int y2_r2 = rec2[3];
 
-          if(x1_p1 == x1_p2 && y1_p1 == y1_p2 && x2_p1 == x2_p2 && y2_p1 == y2_p2)
-          return true;
-          if(x1_p1 >= x2_p2 || y1_p1 >= y2_p2 || x2_p1 <= x1_p2 || y2_p1 <= y1_p2)
-          return false;
+          if(x1_r1 == x1_r2 && y1_r1 == y1_r2 && x2_r1 == x2_r2 && y2_r1 == y2_r2)
+             return true;
+          if(x1_r1 >= x2_r2 || y1_r1 >= y2_r2 || x2_r1 <= x1_r2 || y2_r1 <= y1_r2)
+             return false;
 
           return true;
 
