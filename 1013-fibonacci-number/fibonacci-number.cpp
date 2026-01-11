384e0d5c8b0982a033025f9417b8a4d35cd9f246
Satish <satish64607@gmail.com>
2026-01-11 12:30:50 +0530

    Time: 2 ms (76.99%) | Memory: 7.8 MB (63.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -13,8 +13,19 @@ void helper(vector<int>& v, int i, int n)
         if (n<=1)
 		    return n;
 		
-        int i=0, j=1;
-        vector<int> v(n+1, 0);
+        int a=0, b=1;
+        int c;
+        for(int i=2; i<=n ; i++)
+        {
+             c = a+b;
+             a=b;
+            b=c;
+            //c=b;
+        }
+
+        return c;
+
+        /*vector<int> v(n+1, 0);
         v[0] = 0;
         v[1] = 1;
 
@@ -23,7 +34,7 @@ void helper(vector<int>& v, int i, int n)
             v[i] = v[i-1] + v[i-2];
         }
 
-        return v[n];
+        return v[n];*/
 
        /* helper(v, 2, n);
 
